af8b34e6dfa8427458be99ffc47b17d2cd4bb49e
Prince <merjaprince1020@gmail.com>
2024-11-02 22:52:11 -0700

    Contact page design

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import "./Contact.css";
+import "../components/card.css";
+import "../components/button.css";
 import React from "react";
 
 export default function Contact() {
@@ -11,7 +13,8 @@ export default function Contact() {
     // Explicitly cast form elements to their respective HTML types
     const name = (form.elements.namedItem("name") as HTMLInputElement).value;
     const email = (form.elements.namedItem("email") as HTMLInputElement).value;
-    const message = (form.elements.namedItem("message") as HTMLTextAreaElement).value;
+    const message = (form.elements.namedItem("message") as HTMLTextAreaElement)
+      .value;
     const test = (form.elements.namedItem("test") as HTMLInputElement).value;
 
     const response = await fetch("https://api.web3forms.com/submit", {
@@ -37,35 +40,52 @@ export default function Contact() {
 
   return (
     <>
-      <div className="contact-main-container">
+      <div className="contact">Contact</div>
+      <div className="card contact-main-container">
         <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" required placeholder="Your name" />
-          </div>
-          <div>
-            <label htmlFor="test">Test</label>
-            <input type="text" name="test" required placeholder="test" />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
+          <div className="contact-container">
+            <label htmlFor="name">Name :</label>
             <input
+              className="border"
+              type="text"
+              name="name"
+              required
+              placeholder="Your name"
+            />
+          </div>
+          <div className="contact-container">
+            <label htmlFor="test">Test :</label>
+            <input
+              className="border"
+              type="text"
+              name="Test"
+              required
+              placeholder="test"
+            />
+          </div>
+          <div className="contact-container">
+            <label htmlFor="email">Email :</label>
+            <input
+              className="border"
               type="email"
               name="email"
               required
               placeholder="email@example.com"
             />
           </div>
-          <div>
-            <label htmlFor="message">Message</label>
+          <div className="contact-container">
+            <label htmlFor="message">Message :</label>
             <textarea
+              className="border"
               name="message"
               required
               rows={3}
               placeholder="Enter Message"
             ></textarea>
           </div>
-          <button type="submit">Submit Form</button>
+          <button className="button border" type="submit">
+            Submit Form
+          </button>
         </form>
       </div>
     </>
