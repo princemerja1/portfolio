33e95e7b14a2c9f077d88c45d021f9d0ed051b63
Prince <merjaprince1020@gmail.com>
2024-12-27 11:26:18 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
       <Analytics />
       <SpeedInsights />
       <html lang="en">
+      <meta name="google-site-verification" content="nw3QmgKz104UjN3gtyJ-wArZ3OflW3SsQP5KcrbnXi4" />
         <meta name="author" content="Prince Merja" />
         <body className={`antialiased z-9`}>
           <section className="-z-50">
