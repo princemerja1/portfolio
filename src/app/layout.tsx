5d263743245862c7462588cc8fc05bfff5015ab1
Prince <merjaprince1020@gmail.com>
2024-12-31 15:21:59 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
           content="P2 Vortex, Prince Merja, personal portfolio, game developer, CGI artist, photo editor, video editor, programmer, P2 Vortex portfolio, Prince Merja portfolio, Personal portfolio, Game developer portfolio, CGI artist portfolio, VFX artist portfolio, Programmer portfolio, Photo editor portfolio, Video editor portfolio, Visual effects portfolio, VFX/CGI portfolio, Unreal Engine developer, Game development portfolio, 3D modeling portfolio, VFX design portfolio, 3D artist portfolio, Visual effects design, 3D rendering portfolio, Animation portfolio, CGI movie portfolio, VFX for games, Personal portfolio of a game developer and CGI artist, 3D modeling, VFX, and game development portfolio, Creative portfolio for VFX artists and programmers, Professional portfolio for a CGI artist and programmer, Showcasing innovative projects in VFX and game development,"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <body className={antialiased z-9}>
+        
+        <body className={"antialiased z-9"}>
           <section className="-z-50">
             <span className="star"></span>
             <span className="star"></span>
