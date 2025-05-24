8855d14d5e2bc8f6257c055b7ca78afdeffea426
Prince <89233327+princemerja1@users.noreply.github.com>
2025-05-24 13:31:19 +0530

    Update page.tsx

--- src/app/thankyou/page.tsx
+++ src/app/thankyou/page.tsx
@@ -1,32 +1,18 @@
-
-"use client";
 // app/thankyou/page.tsx
-import type { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "Thank You | Prince Merja",
-  description: "Submission successful — Prince Merja will get back to you shortly.",
-  alternates: {
-    canonical: "https://www.princemerja.me/thankyou",
-  },
-};
 import "./thankyou.css";
 import "../components/button.css";
-import React from "react";
 import { useRouter } from "next/navigation";
 
 export default function ThankYou() {
   const router = useRouter();
 
-  const handleHome = () => {
-    router.push("/");
-  };
-
   return (
     <div className="thank-you-container">
       <h1>Thank You!</h1>
       <p>Your submission was successful. We will get back to you shortly.</p>
-      <button className="button" onClick={handleHome}>
+      <button className="button" onClick={() => router.push("/")}>
         Go to Home
       </button>
     </div>
